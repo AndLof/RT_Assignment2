fba8f48b09b209fcf8c3e54af283230383001175
AndLof <andrea.loforese96@gmail.com>
2024-12-30 10:20:23 +0000

    fifth commit

--- scripts/action_client.py
+++ scripts/action_client.py
@@ -5,7 +5,7 @@ import actionlib
 from actionlib import GoalStatus
 from nav_msgs.msg import Odometry
 from assignment2_rt.msg import Custom_info
-from assignment_2_2024.msg import PlanningAction, PlanningGoal
+from assignment_2_2024.msg import PlanningAction, PlanningGoal, PlanningActionResult
 
 def odom_callback(msg):
     global pub
@@ -16,6 +16,39 @@ def odom_callback(msg):
     custom_msg.vel_z = msg.twist.twist.angular.z
     pub.publish(custom_msg)
 
+def result_converter(current_result):
+	if current_result == 0:
+		converted_result = "PENDING"
+	elif current_result == 1:
+		converted_result = "ACTIVE"
+	elif current_result == 2:
+		converted_result = "PREEMPTED"
+	elif current_result == '3':
+		converted_result = "SUCCEEDED"
+	elif current_result == 4:
+		converted_result = "ABORTED"
+	elif current_result == 5:
+		converted_result = "REJECTED"
+	elif current_result == 6:
+		converted_result = "PREEMPTING"
+	elif current_result == 7:
+		converted_result = "RECALLING"
+	elif current_result == 8:
+		converted_result = "RECALLED"
+	elif current_result == 9:
+		converted_result = "LOST"
+
+
+
+def result_callback(msg):
+	global current_result
+	global converted_result
+	converted_result = "Test"
+#	current_result = PlanningActionResult()
+	current_result = msg.status.status
+	converted_result = result_converter(current_result)
+	rospy.loginfo(f"State of goal: {current_result}" + "%s", converted_result)
+
 
 def main():
     global pub
@@ -25,6 +58,8 @@ def main():
     pub = rospy.Publisher('/custom_info', Custom_info, queue_size=10)
     #rospy.wait_for_message('/odom', Odometry)
     rospy.Subscriber('/odom', Odometry, odom_callback)
+    
+    rospy.Subscriber('/reaching_goal/result', PlanningActionResult, result_callback)
 
     #rospy.spin()
     
